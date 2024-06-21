720556d13b965f4efd754d0ca0ef37deeaff33c9
Neo-Getsu <damien.devinfo@gmail.com>
2024-06-21 23:48:27 +0200

    Update  zpp.js for production

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ import logsBanRoute from "./routes/logsBanRoute";
 import {errorHandler} from "./middleware/errorMiddleware";
 import {NotFoundError} from "./classes/Error";
 import logsSocialsRoute from "./routes/logsSocialsRoute";
+import chalk from "chalk";
 
 
 
@@ -30,6 +31,11 @@ app.use("/api/v1/indexes", indexRoute);
 
 
 app.use((req, res, next) => {
+    console.log(chalk.green('===================='));
+    console.log(chalk.blue('URL de la requête:'), chalk.yellow(req.url));
+    console.log(chalk.blue('Adresse IP de l\'auteur:'), chalk.yellow(req.ip));
+    console.log(chalk.blue('Query:'), chalk.yellow(JSON.stringify(req.query)));
+    console.log(chalk.green('===================='));
     next(new NotFoundError());
 });
 app.use(errorHandler);
