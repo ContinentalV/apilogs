504a41a2246db4609af49fa9a650a34b92a54b99
Neo-Getsu <damien.devinfo@gmail.com>
2024-06-21 17:38:17 +0200

    Update index.js for production

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,25 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import morgan, {TokenIndexer } from "morgan";
+import morgan from "morgan";
 import app from './app';
-
-dotenv.config();
 import mongoose from 'mongoose';
 import dbConnect from "./config/database";
 import logsRoute from "./routes/logsRoute";
 
+dotenv.config();
 
-
-const port = 3000;
-
+const port = process.env.PORT || 3000;
+const isProduction = process.env.NODE_ENV === 'production';
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Bonjour, Maître Getsu le Grand!');
 });
 
-
-
 dbConnect().then(() => {
-    app.listen(port, () => {
-        console.log(`▪️Serveur en cours d'exécution sur http://localhost:${port}`);
+    app.listen(Number(port), isProduction ? '0.0.0.0' : 'localhost', () => {
+        console.log(`▪️Serveur en cours d'exécution sur http://${isProduction ? '0.0.0.0' : 'localhost'}:${port}`);
         console.log('▪️Base de données connectée avec succès✅✅');
-    });
-}).catch((error:any) => {
+    })
+}).catch((error: any) => {
     console.error('Impossible de démarrer le serveur:', error);
 });
