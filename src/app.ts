640196211a7dfdc7c8923805604299549dd3f1fb
Neo-Getsu <damien.devinfo@gmail.com>
2024-06-22 15:17:27 +0200

    verif ip from

--- src/app.ts
+++ src/app.ts
@@ -31,9 +31,15 @@ app.use("/api/v1/indexes", indexRoute);
 
 
 app.use((req, res, next) => {
+    let ip = (typeof req.headers['x-forwarded-for'] === 'string'
+        ? req.headers['x-forwarded-for']
+        : req.headers['x-forwarded-for']?.[0]) || req.socket.remoteAddress || '';
+    if (ip && ip.substr(0, 7) == "::ffff:") {
+        ip = ip.substr(7)
+    }
     console.log(chalk.green('===================='));
     console.log(chalk.blue('URL de la requête:'), chalk.yellow(req.url));
-    console.log(chalk.blue('Adresse IP de l\'auteur:'), chalk.yellow(req.ip));
+    console.log(chalk.blue('Adresse IP de l\'auteur:'), chalk.yellow(ip));
     console.log(chalk.blue('Query:'), chalk.yellow(JSON.stringify(req.query)));
     console.log(chalk.green('===================='));
     next(new NotFoundError());
